9eb2a600039d0ac8b9972be73192e907dc551380
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2023-12-26 21:08:30 +0545

    added backend

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GuardianSheild
 A fraud detecting web extension
+
+
+[drive link for summarizing model](https://drive.google.com/drive/folders/19i-DZlmMfI545vtS8iQ9mkZLsmvKP_rY?usp=sharing)
---
+++ backend/app.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, HTTPException
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+class SummarizationRequest(BaseModel):
+    text: str
+
+
+class SummarizationResponse(BaseModel):
+    summary: str
+
+class InputText(BaseModel):
+    text: str
+
+tokenizer_mail = AutoTokenizer.from_pretrained("bert-base-uncased")
+tokenizer_summarize = AutoTokenizer.from_pretrained("tokenizer_summarize")
+model_mail = AutoModelForSequenceClassification.from_pretrained('./model_mail')
+model_summarize = AutoModelForSequenceClassification.from_pretrained('./model_summarize')
+
+@app.post("/predict")
+def predict_spam_ham(item: InputText):
+    inputs = tokenizer_mail(item.text, return_tensors="pt")
+    with torch.no_grad():
+        outputs = model_mail(**inputs)    
+    predicted_percentage = outputs.logits
+    return {"text": predicted_percentage}
+
+@app.post("/summarize")
+async def summarize(request: SummarizationRequest):
+    try:
+        inputs = tokenizer_summarize(request.text, max_length=1024, truncation=True, return_tensors="pt")
+        summary_ids = model_summarize.generate(inputs["input_ids"], max_length=128, length_penalty=0.8, num_beams=8)
+        summary = tokenizer_summarize.decode(summary_ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
+
+        return SummarizationResponse(summary=summary)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
