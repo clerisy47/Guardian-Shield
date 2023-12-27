332516cc1f27948e80b022a4626e35eef2c89c74
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2023-12-27 21:13:21 +0545

    change shutil module to os module

--- download_models.py
+++ download_models.py
@@ -1,6 +1,6 @@
 import gdown
 import zipfile
-import shutil
+import os
 
 
 url = 'https://drive.google.com/uc?export=download&id=1vgjA18cNYKzl87tIssqSC505xw6Hyr71'
@@ -10,6 +10,6 @@ gdown.download(url, output, quiet=False)
 with zipfile.ZipFile("models.zip", 'r') as zip_ref:
     zip_ref.extractall("models")
 
-shutil.rmtree("models.zip")
+os.remove("models.zip")
 
 
