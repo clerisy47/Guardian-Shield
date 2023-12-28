2f80be2b07dcc80542f93f0ee25852b72e85513d
Darshan Poudel <pranishpoudel10@gmail.com>
2023-12-28 08:23:45 +0545

    final

--- Extension/content.js
+++ Extension/content.js
@@ -29,6 +29,7 @@ const hasTermsAndConditions = () => {
   }
   return false;
 };
+
 const scrapeTermsAndConditions = () => {
   const tandc = document.querySelector("#primary");
   if (tandc) {
@@ -36,28 +37,126 @@ const scrapeTermsAndConditions = () => {
   }
 };
 
-const isPhising = (url) => {
-  targetUrl = "http://fakebook/login.html";
-  return url === targetUrl;
-};
+function isPotentialPhishing(url) {
+  // official URLS
+  const safeUrls = [
+    "https://www.facebook.com",
+    "https://www.instagram.com",
+    "https://www.twitter.com",
+  ];
+
+  // Calculating similarity percentage
+  let similarityPercentage;
+  for (let i = 0; i < safeUrls.length; i++) {
+    similarityPercentage = calculateStringSimilarity(url, facebookUrl);
+    if (similarityPercentage > 80) {
+      break;
+    }
+  }
+
+  // threshold for potential phising
+  const threshold = 80;
+
+  // Checking if the similarity percentage is above the threshold
+  return similarityPercentage >= threshold;
+}
+
+function calculateStringSimilarity(str1, str2) {
+  const longerStr = str1.length > str2.length ? str1 : str2;
+  const shorterStr = str1.length > str2.length ? str2 : str1;
+
+  const longerLength = longerStr.length;
+  if (longerLength === 0) {
+    return 100.0;
+  }
+
+  const similarity =
+    ((longerLength - editDistance(longerStr, shorterStr)) /
+      parseFloat(longerLength)) *
+    100;
+  return similarity;
+}
+
+function editDistance(str1, str2) {
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+
+  const costs = new Array();
+  for (let i = 0; i <= str1.length; i++) {
+    let lastValue = i;
+    for (let j = 0; j <= str2.length; j++) {
+      if (i === 0) {
+        costs[j] = j;
+      } else if (j > 0) {
+        let newValue = costs[j - 1];
+        if (str1.charAt(i - 1) !== str2.charAt(j - 1)) {
+          newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+        }
+        costs[j - 1] = lastValue;
+        lastValue = newValue;
+      }
+    }
+    if (i > 0) {
+      costs[str2.length] = lastValue;
+    }
+  }
+  return costs[str2.length];
+}
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log(request.url);
   if (request.pageChanged || request.action) {
+    //Runs if user opens mail or clicks icon on mail tab
     if (isMail(request.url)) {
-      const data = scrapeEmailsFromPage();
-      //Check for spam or ham
-      let spam = 1;
-      if (spam) {
-        alert(
-          "WARNING: GUARDIAN SHEILD has detected potential fraudulent activity associated with this email!\n 1.  Avoid clicking links or downloading attachments.\n 2.  Do not provide personal information"
-        );
-      }
+      const content = scrapeEmailsFromPage();
+      const data = {
+        text: content,
+      };
+      const url = "http://localhost:8000/predict";
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          //High probability of being fraud!
+          if (data.text > 50) {
+            alert(
+              "WARNING: GUARDIAN SHEILD has detected potential fraudulent activity associated with this email!\n 1.  Avoid clicking links or downloading attachments.\n 2.  Do not provide personal information"
+            );
+          } else {
+            alert(
+              `This mail has only ${data.text}% probability of being fraud/scam.`
+            );
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
     } else if (hasTermsAndConditions(request.url)) {
-      const data = scrapeTermsAndConditions();
-      let summary = "get summary";
-      alert("GUARDIAN SHEILD SUMMARIZER\n\nSummary.");
-    } else if (isPhising(request.url)) {
+      const content = scrapeTermsAndConditions();
+      const data = { text: content };
+      const url = "http://localhost:8000/summarize";
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          //Summarizing terms and conditions
+          alert(`GUARDIAN SHEILD SUMMARIZER\n\n${data.summary}.`);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    } else if (isPotentialPhishing(request.url)) {
+      //Potential Phising
       alert(
         "We have identified this website as a potential phishing site. Your security is our top priority. Please take immediate action:\n1. **Do not enter any personal information.**\n2. **Do not proceed or interact with the site.**\n3. **Close this tab or browser immediately.**"
       );
--- app.py
+++ app.py
@@ -9,31 +9,39 @@ app = FastAPI()
 
 
 tokenizer_mail = AutoTokenizer.from_pretrained("bert-base-uncased")
-tokenizer_summarize = AutoTokenizer.from_pretrained("./models/tokenizer_summarize")
-model_mail = AutoModelForSequenceClassification.from_pretrained('./models/model_spam')
-model_summarize = AutoModelForSeq2SeqLM.from_pretrained('./models/model_summarize')
+tokenizer_summarize = AutoTokenizer.from_pretrained(
+    "./models/tokenizer_summarize")
+model_mail = AutoModelForSequenceClassification.from_pretrained(
+    './models/model_spam')
+model_summarize = AutoModelForSeq2SeqLM.from_pretrained(
+    './models/model_summarize')
+
 
 @app.post("/predict")
 def predict_spam_ham(text: str):
     inputs = tokenizer_mail(text, return_tensors="pt")
     with torch.no_grad():
-        outputs = model_mail(**inputs)    
+        outputs = model_mail(**inputs)
     logits = outputs.logits
     probabilities = F.softmax(logits, dim=1)
     spam_probability = probabilities[0][0].item()
     print({"text": round(spam_probability*100, 2)})
     return {"text": round(spam_probability*100, 2)}
 
+
 @app.post("/summarize")
 async def summarize(text: str):
     try:
         inputs = tokenizer_summarize(text, return_tensors="pt")
         with torch.no_grad():
-            outputs = model_summarize.generate(**inputs, min_length=len(text)//10)
-        summary = tokenizer_summarize.decode(outputs[0], skip_special_tokens=True)
+            outputs = model_summarize.generate(
+                **inputs, min_length=len(text)//10)
+        summary = tokenizer_summarize.decode(
+            outputs[0], skip_special_tokens=True)
         return {"summary": summary}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error during summarization: {str(e)}")
-    
+        raise HTTPException(
+            status_code=500, detail=f"Error during summarization: {str(e)}")
+
 if __name__ == "__main__":
     uvicorn.run(app, port=8000)
