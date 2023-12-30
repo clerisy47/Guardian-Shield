f7e03a015ab9cc16f75f3c31f7c35025d31505a8
Utsav Acharya <91122820+clerisyutsav47@users.noreply.github.com>
2023-12-30 15:51:46 +0545

    Add pydantic base model for args

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, AutoModelForSeq2SeqLM
@@ -5,6 +6,11 @@ import torch
 import uvicorn
 import torch.nn.functional as F
 
+class PredictRequest(BaseModel):
+    text: str
+
+class SummarizeRequest(BaseModel):
+    text: str
 
 app = FastAPI()
 app.add_middleware(
@@ -17,41 +23,34 @@ app.add_middleware(
 
 # Loads pretrained models and tokenizers
 tokenizer_mail = AutoTokenizer.from_pretrained("bert-base-uncased")
-tokenizer_summarize = AutoTokenizer.from_pretrained(
-    "./models/tokenizer_summarize")
-model_mail = AutoModelForSequenceClassification.from_pretrained(
-    './models/model_spam')
-model_summarize = AutoModelForSeq2SeqLM.from_pretrained(
-    './models/model_summarize')
-
+tokenizer_summarize = AutoTokenizer.from_pretrained("./models/tokenizer_summarize")
+model_mail = AutoModelForSequenceClassification.from_pretrained('./models/model_spam')
+model_summarize = AutoModelForSeq2SeqLM.from_pretrained('./models/model_summarize')
 
 # Endpoint for predicting spam or ham
 @app.post("/predict")
-def predict_spam_ham(text: str):
-    inputs = tokenizer_mail(text, return_tensors="pt")
+def predict_spam_ham(request: PredictRequest):
+    inputs = tokenizer_mail(request.text, return_tensors="pt")
     with torch.no_grad():
         outputs = model_mail(**inputs)
     logits = outputs.logits
     probabilities = F.softmax(logits, dim=1)
     spam_probability = probabilities[0][0].item()
-    print({"text": round(spam_probability*100, 2)})
-    return {"text": round(spam_probability*100, 2)}
-
+    return {"text": round(spam_probability * 100, 2)}
 
 # Endpoint for summarizing text
 @app.post("/summarize")
-async def summarize(text: str):
+async def summarize(request: SummarizeRequest):
     try:
-        inputs = tokenizer_summarize(text, return_tensors="pt")
+        inputs = tokenizer_summarize(request.text, return_tensors="pt")
         with torch.no_grad():
-            outputs = model_summarize.generate(
-                **inputs, min_length=len(text)//10)
-        summary = tokenizer_summarize.decode(
-            outputs[0], skip_special_tokens=True)
+            outputs = model_summarize.generate(**inputs, min_length=len(request.text)//10)
+        summary = tokenizer_summarize.decode(outputs[0], skip_special_tokens=True)
         return {"summary": summary}
     except Exception as e:
         raise HTTPException(
-            status_code=500, detail=f"Error during summarization: {str(e)}")
+            status_code=500, detail=f"Error during summarization: {str(e)}"
+        )
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000)
